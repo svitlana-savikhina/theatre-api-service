b46743032ed042e0d70723677420ace97dc08926
Svitlana Savikhina <savihina111@gmail.com>
2023-10-17 15:56:13 +0300

    Fix bug in models.py

--- theatre/models.py
+++ theatre/models.py
@@ -30,7 +30,7 @@ class Genre(models.Model):
 
 def play_image_file_path(instance, filename):
     _, extension = os.path.splitext(filename)
-    filename = f"{slugify(instance.info)} - {uuid.uuid4()}.{extension}"
+    filename = f"{slugify(instance.title)} - {uuid.uuid4()}.{extension}"
     return os.path.join("uploads/plays/", filename)
 
 
