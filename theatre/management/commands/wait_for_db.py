4981f41f7e44ca90e011e9168536e5e8e81d778a
Svitlana Savikhina <savihina111@gmail.com>
2023-10-31 11:55:25 +0200

    Fix bug in wait_for_db.py

--- theatre/management/commands/wait_for_db.py
+++ theatre/management/commands/wait_for_db.py
@@ -2,7 +2,7 @@ import time
 
 from django.core.management.base import BaseCommand
 from django.db.utils import OperationalError
-from django.db import connection
+from django.db import connections
 
 
 class Command(BaseCommand):
@@ -14,10 +14,9 @@ class Command(BaseCommand):
         db_connection = None
         while not db_connection:
             try:
-                connection.ensure_connection()
-                db_connection = True
+                db_connection = connections["default"]
             except OperationalError:
-                self.stdout.write("Database unavailable, waiting 2 second...")
-                time.sleep(2)
+                self.stdout.write("Database unavailable, waiting 1 second...")
+                time.sleep(1)
 
         self.stdout.write(self.style.SUCCESS("Database available!"))
